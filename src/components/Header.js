7a974fa85c88229919095cda4d73889d805a6ed4
Drapala <joao.drapala@gmail.com>
2021-02-21 18:23:22 -0300

    header

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import ListProducts from "../components/ListProducts";
+ 
+const Header = () => {
+    return (
+        <header
+            className={'header'}>
+            <div className={'header__logo'}>
+                <span>S h o p e e</span>
+            </div>
+            <div className={'header__search'}>
+                <div className={'search'}>
+                    <input
+                        className={'search__input'}
+                        type={'search'}
+                        placeholder={'Buscar....'}
+                        placeholder={'Buscar....'}
+                        results='0' />
+                </div>
+            </div>
+            <div className={'header__cart'}>
+
+            </div>
+            <ListProducts></ListProducts>
+        </header>
+    );
+}
+
+export default Header;
