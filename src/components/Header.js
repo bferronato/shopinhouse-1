e7ee51eb510f90149beae83848185c2f0bf840be
Drapala <joao.drapala@gmail.com>
2021-02-21 18:14:18 -0300

    Commits no header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
+import React from 'react'
+
 const Header = () => {
     return (
         <header
             className={'header'}>
             <div className={'header__logo'}>
-                <span>SHOPEE</span>
+                <span>S h o p e e</span>
             </div>
             <div className={'header__search'}>
                 <div className={'search'}>
                     <input
                         className={'search__input'}
-                        type={'text'} />
+                        type={'search'}
+                        placeholder={'Buscar....'}
+                        results='0' />
                 </div>
             </div>
             <div className={'header__cart'}>
