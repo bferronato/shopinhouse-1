a165bbd748c53d3728de1388b36183bc6adee1d5
Bruno Henrique Ferronato <bferronato@gmail.com>
2021-02-22 00:42:25 -0300

    Carrinho de compras

--- src/components/ShopCart.js
+++ src/components/ShopCart.js
@@ -6,7 +6,30 @@ const ShopCart = () => {
     return (
         <HomeView title="Carrinho de compras">
             <div className='shop__cart'>
-                ok
+                <table className="table__cart__list">
+                    <tr>
+                        <th></th>
+                        <th>Preço UN.</th>
+                        <th>Quantidade</th>
+                        <th>Subtotal</th>
+                    </tr>
+                    <tr>
+                        <td>Parafusadeira automática Britânia</td>
+                        <td>249,00</td>
+                        <td>1</td>
+                        <td>249,00</td>
+                    </tr>
+                    <tr>
+                        <td>Parafusadeira automática Britânia</td>
+                        <td>17,90</td>
+                        <td>2</td>
+                        <td>35,80</td>
+                    </tr>
+                </table>
+                <div className="table__cart__total">
+                    <span>R$ 284,80</span>
+                    <button>Finalizar Compra</button>
+                </div>
             </div>
         </HomeView>
     );
