a5d2373dedc1cc850d5fe63107f16d4305e33072
Bruno Henrique Ferronato <bferronato@gmail.com>
2021-02-23 00:25:25 -0300

    Layout carrinho de compras

--- src/components/ShopCart.js
+++ src/components/ShopCart.js
@@ -6,29 +6,48 @@ const ShopCart = () => {
     return (
         <HomeView title="Carrinho de compras">
             <div className='shop__cart'>
-                <table className="table__cart__list">
-                    <tr>
-                        <th></th>
-                        <th>Preço UN.</th>
-                        <th>Quantidade</th>
-                        <th>Subtotal</th>
-                    </tr>
-                    <tr>
-                        <td>Parafusadeira automática Britânia</td>
-                        <td>249,00</td>
-                        <td>1</td>
-                        <td>249,00</td>
-                    </tr>
-                    <tr>
-                        <td>Parafusadeira automática Britânia</td>
-                        <td>17,90</td>
-                        <td>2</td>
-                        <td>35,80</td>
-                    </tr>
-                </table>
-                <div className="table__cart__total">
-                    <span>R$ 284,80</span>
-                    <button>Finalizar Compra</button>
+                <div className="cart__container">
+                    <div className="cart__list__header">
+                        <div>&nbsp;</div>
+                        <div className="cart__list__header__title">
+                            <div>Preço UN.</div>
+                            <div>Quantidade</div>
+                            <div>Subtotal</div>
+                        </div>
+                    </div>
+
+                    <div className="cart__list__body__panel">
+                        <div className="cart__list__body">
+                            <div className="cart__list__body__product__title">
+                                Parafusadeira automática Britânia
+                            </div>
+                            <div className="cart__list__body__product">
+                                <div>249,00</div>
+                                <div>1</div>
+                                <div>249,00</div>
+                            </div>
+                        </div>
+                        <div className="cart__list__body__divider"></div>
+                        <div className="cart__list__body">
+                            <div className="cart__list__body__product__title">
+                                Outro produto
+                            </div>
+                            <div className="cart__list__body__product">
+                                <div>17,90</div>
+                                <div>2</div>
+                                <div>35,80</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="cart__list__footer">
+                        <div>&nbsp;</div>
+                        <div className="cart__list__footer__info">
+                            <p className="cart__list__footer__info__subtotal">R$ 284,80</p>
+                            <button className="cart__list__footer__info__button">Finalizar Compra</button>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </HomeView>
