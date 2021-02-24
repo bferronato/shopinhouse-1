5a20ca99202019445acfed91ad3aef3fca9f80c5
Notnox <74467549+Notnox@users.noreply.github.com>
2021-02-24 14:21:06 -0300

    Ajustes no Card
    -Refatorando o código do Card.
    -Desmembrando os campos dos botões do card.
    -Ajustado estilo do ListProduct.
    -Adicionado titulo de Bem vindo! da página inicial.

--- src/components/ListProducts.js
+++ src/components/ListProducts.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, Fragment } from "react"
 import { fetchProducts } from "../services/api"
 import Card from "../components/Card"
 import '../styles/ListProducts.css'
@@ -6,65 +6,21 @@ import '../styles/ListProducts.css'
 const ListProducts = () => {
 
     const [products, setProducts] = useState([]);
-    const [activeCard, setActiveCard] = useState('')
-
-    const [bag, setBag] = useState([])
-    const [orderBag, setOrderbag] = useState('')
-    const [operation, setOperation] = useState('')
 
     fetchProducts().then(function (result) {
         setProducts(result)
     });
 
-    useEffect(() => {
-        const amountBag = []
-
-        products.map((item => {
-            amountBag.push({id: item.sku, amount: 0})
-        }))
-        setBag(amountBag)
-    },[products])
-
-    useEffect(() => {
-        const preventCard = bag
-        for (const item in preventCard) {
-            if (preventCard[item].id === orderBag) {
-                if (operation === 'sum') {
-                    preventCard[item].amount += 1
-                } else { preventCard[item].amount -= 1 }
-                setBag(preventCard)
-                break
-            }
-        }
-        setOrderbag(0)
-    }, [orderBag]) 
-    /* const bagOrderId = (id) => {
-        let amountBag = {}
-        bag.map((item => {
-            if (id === item.id) {
-                amountBag = {id: id, amount: item.amount}
-                return amountBag
-            }
-        }))
-    } */
-
-    {/* <HomeView title="Bem vindo!">
-        <div className='list__products'>
-            {products.map((product, i) => (
-                <Card product={product} key={i}></Card>
-            ))}
-        </div>
-    </HomeView> */}
     return (
-        <div className='listProducts'>
-            {products.map((item, i) => {
-                if (activeCard.length === 0) {
-                    return <Card product={item} key={i} bagOrder={bag} setOrderbag={setOrderbag} setOperation={setOperation} setActiveCard={setActiveCard} />
-                } else if (item.sku === activeCard) {
-                    return <Card product={item} key={i} bagOrder={bag} setOrderbag={setOrderbag} setOperation={setOperation} setActiveCard={setActiveCard} />
-                }
-            })}
-        </div>
+        <Fragment>
+            <h1 className="ListProducts__title">Bem vindo!</h1>
+            <div className='listProducts'>
+
+                {products.map((item) => {
+                    return <Card key={item.sku} product={item} />
+                })}
+            </div>
+        </Fragment>
     );
 }
 
---
+++ src/components/PurchaseButtons.js
@@ -0,0 +1,11 @@
+<footer>
+                <button className="Card__footer__button__buy">Comprar</button>
+                    {/* {amount() < 1 && }
+                    {amount() >= 1 &&
+                        <Fragment>
+                            <button style={activeDesc ? Card__footer__amountBag__sub__small : Card__footer__amountBag__sub__medium} className="Card__footer__amountBag__sub" onClick={backButton}>{amount() > 1 ? "-" : <FaTrash />}</button>
+                            <span style={activeDesc ? Card__footer__amountBag_small : Card__footer__amountBag_medium} className="Card__footer__amountBag">{amount()}</span>
+                            <button style={activeDesc ? Card__footer__amountBag__sum__small : Card__footer__amountBag__sum__medium} className="Card__footer__amountBag__sum" onClick={buyButton}>+</button>
+                        </Fragment>
+                    } */}
+                </footer>
