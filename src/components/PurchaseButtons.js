7a537d5e69a69759915b719a5aa100ef987e8526
Notnox <llrricardo@gmail.com>
2021-02-25 16:03:49 -0300

    Funcionalidades novas nos botões de compra.
    -Reconhece quando um produto é adicionado ao carrinho.
    -Adiciona uma quantidade do produto no redux
    -Reduz uma quantidade do produto no redux

--- src/components/PurchaseButtons.js
+++ src/components/PurchaseButtons.js
@@ -1,33 +1,63 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import '../styles/PurchaseButtons.css'
+import { FaTrash } from 'react-icons/fa'
 import { useSelector, useDispatch } from 'react-redux';
-import { addToCart } from '../redux/product/productAction'
+import { addToCart, incrementAmount, decrementAmount } from '../redux/product/productAction'
 import { getCart } from '../redux/product/productSelector'
 
 const PurchaseButtons = ({ product }) => {
 
     const dispatch = useDispatch();
-
     const cart = useSelector(getCart)
 
     function handleAdd(product) {
         dispatch(addToCart(product))
     };
 
-    
-        useEffect(() => {
-            console.log(cart);
-        }, [cart])
+    function handleIncrement(product) {
+        dispatch(incrementAmount(Number(product.id)))
+    }
+
+    function handleDecrement(product) {
+        dispatch(decrementAmount(Number(product.id)))
+    }
+
+    const firstRequest = () => {
+        let request = true
+
+        cart.map((item) => {
+            if (item.id === product.id) { request = false }
+        })
+
+        return request
+    }
+
+    const amountCont = () => {
+        let amount = 0
+
+        cart.map((item) => {
+            if (item.id === product.id) { amount = item.amountCont }
+        })
+
+        return amount
+    }
+
+    useEffect(()=> {
+        console.log(cart);
+    },[cart])
+
 
     return (
         <div className="purchaseButtons__body">
-            {true && <button className="Card__footer__button__buy" onClick={() => handleAdd(product)}>Comprar</button>}
-               {/* /*  <Fragment>
-                    <button style={activeDesc ? Card__footer__amountBag__sub__small : Card__footer__amountBag__sub__medium} className="Card__footer__amountBag__sub" onClick={backButton}>{amount() > 1 ? "-" : <FaTrash />}</button>
-                    <span style={activeDesc ? Card__footer__amountBag_small : Card__footer__amountBag_medium} className="Card__footer__amountBag">{amount()}</span>
-                    <button style={activeDesc ? Card__footer__amountBag__sum__small : Card__footer__amountBag__sum__medium} className="Card__footer__amountBag__sum" onClick={buyButton}>+</button>
-                </Fragment> */ }
-            
+            {firstRequest() && <button className="Card__footer__button__buy" onClick={() => handleAdd(product)}>Comprar</button>}
+            {!firstRequest() &&
+                <Fragment>
+                    <button className="Card__footer__amountBag__sub" onClick={() => handleDecrement(product)}>{amountCont() > 1 ? "-" : <FaTrash />}</button>
+                    <span className="Card__footer__amountBag">{amountCont()}</span>
+                    <button  className="Card__footer__amountBag__sum" onClick={() => handleIncrement(product)}>+</button>
+                </Fragment>
+            }
+
         </div>
     );
 };
