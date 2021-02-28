0acbdf0da6dcc008051851c9d26f4292826c15b7
Bruno Henrique Ferronato <bferronato@gmail.com>
2021-02-27 23:02:00 -0300

    Inclusão baseView pagina de detalhes

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,5 +1,6 @@
 import { useState, useEffect, Fragment } from 'react';
 import { fetchProducts } from '../services/api';
+import BaseView from '../views/BaseView';
 import CardDetail from '../components/CardDetail';
 import '../styles/ProductDetail.css';
 import { getDetailingProduct } from '../redux/product/productSelector';
@@ -11,11 +12,12 @@ const ProductDetail = () => {
 
   return (
     <Fragment>
-      <h1 className="ProductDetail__title">{product.name}</h1>
-      <div className="ProductDetail">
-        <CardDetail key={product} product={product}></CardDetail>
-        <ReturnButton />
-      </div>
+      <BaseView title={product.name}>
+        <div className="ProductDetail">
+          <CardDetail key={product} product={product}></CardDetail>
+          <ReturnButton />
+        </div>
+      </BaseView>
     </Fragment>
   );
 };
