e4d014a59547be151946e2b5bc063b7978990017
João Francisco Drapala <joao.drapala@gmail.com>
2021-02-28 19:51:36 -0300

    filtro de busca implementado

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { getCartAmout } from '../redux/product/productSelector';
-import './Navbar.css';
+import { getCartAmout, getIsLoading } from '../redux/product/productSelector';
+import '../styles/Navbar.css';
 import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { asyncLoadQuery } from '../redux/product/productAction';
 
 function Navbar(props) {
-  const [search, setSearch] = useState('');
+  const [pesquisa, setPesquisa] = useState("")
+  const [titulo, setTitulo] = useState("Bem vindo")
   const history = useHistory();
-
   const carAmount = useSelector(getCartAmout);
+  const isLoading = useSelector(getIsLoading);
+  const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    if (pesquisa.length === 0) {setTitulo("Bem vindo")} else {setTitulo(`Resultados para "${pesquisa}"`)
+    }} ,[pesquisa])
+
 
   return (
     <div className="Navbar">
@@ -17,18 +26,19 @@ function Navbar(props) {
       <div className="firstThird">
         <div className="logo">
           <a onClick={() => history.push('/')}>Shopee</a>
-          <div className="feedback">Resultados para {search}</div> { /* Precisa deixar o valor default como "Bem vindo!" e renderizar o feedback somente na view principal/homepage */}
+          <div className="feedback">Resultados para {pesquisa}</div> { /* Precisa deixar o valor default como "Bem vindo!" e renderizar o feedback somente na view principal/homepage */}
         </div>
+        <div>{titulo}</div>
       </div>
 
       <div className="secondThird">
         <div className="search">
           <input
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
+            value={pesquisa}
+            onChange={(e) => setPesquisa(e.target.value)}
             placeholder="Buscar...."
           ></input>
-          <span className="material-icons">search</span>
+          <span className="material-icons" onClick={ ()=> dispatch(asyncLoadQuery(pesquisa)) } >search</span>
         </div>
       </div>
 
