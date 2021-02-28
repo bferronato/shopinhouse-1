d32c5644e2762fbac41a17e486c4b50526e90fb2
Brayan Moncks <brayanbmn@hotmail.com>
2021-02-27 22:10:13 -0300

    Ajuste de CSS product detail, criado ReturnButton

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,39 @@
-import React from 'react'
+import React from 'react';
 import { useHistory } from 'react-router-dom';
-import './Navbar.css'
+import './Navbar.css';
 import { useState } from 'react';
 
 function Navbar(props) {
+  const [search, setSearch] = useState('');
+  const history = useHistory();
 
-    const [search, setSearch] = useState('')
-    const history = useHistory();
-
-    return (
-        <div className="Navbar">
-            <div className="firstThird">
-                <div className="logo">
-                    <a onClick={() => history.push("/") }>Shopee</a>
-                </div>
-            </div>
-            <div className="secondThird">
-                <div className="search">
-                    <input value={search} onChange={e => setSearch(e.target.value)} >
-                    </input>
-                    <div>Resultados para {search}</div>
-                    <span className="material-icons">
-                        search
-                    </span>
-                </div>
-            </div>
-            <div className="thirdThird">
-                <div className="cart">
-                    <button onClick={() => history.push("/shop-cart") } type="Button">
-                        <span className="material-icons">
-                            shopping_cart
-                        </span>
-                        <span className="notification">
-                            9
-                        </span>
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className="Navbar">
+      <div className="firstThird">
+        <div className="logo">
+          <a onClick={() => history.push('/')}>Shopee</a>
         </div>
-    )
+      </div>
+      <div className="secondThird">
+        <div className="search">
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          ></input>
+          {/* <div>Resultados para {search}</div> */}
+          <span className="material-icons">search</span>
+        </div>
+      </div>
+      <div className="thirdThird">
+        <div className="cart">
+          <button onClick={() => history.push('/shop-cart')} type="Button">
+            <span className="material-icons">shopping_cart</span>
+            <span className="notification">9</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
